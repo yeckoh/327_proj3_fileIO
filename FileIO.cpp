6e7d8137f8fb8222fbed7cb304a4fa068080dc65
Keith <kperkins411@gmail.com>
2017-10-08 20:08:47 -0400

    init

---
+++ FileIO.cpp
@@ -0,0 +1,24 @@
+/*
+ * FileReader.cpp
+ *
+ *  Created on: Oct 8, 2017
+ *      Author: keith
+ */
+#include <iostream>
+#include <fstream>
+#include "../327_proj3_test/includes/FileIO.h"
+#include "../327_proj3_test/includes/constants.h"
+
+using namespace std;
+
+int KP_FileIO::getFileContents(const std::string &filename, std::string &contents)
+{
+	//TODO fill in
+}
+
+int KP_FileIO::writeVectortoFile(const std::string filename,std::vector<std::string> &myEntryVector)
+{
+	//TODO fill in
+}
+
+
