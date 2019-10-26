4db8a2a96c4c77862baeed2f2b11305645ca6eba
yeckoh <yeckoh@gmail.com>
2019-10-26 09:02:42 -0700

    io completed

--- FileIO.cpp
+++ FileIO.cpp
@@ -1,54 +1,45 @@
-/*
- * FileReader.cpp
- *
- *  Created on: Oct 8, 2017
- *      Author: keith
- */
-#include <iostream>
+/*============================================================================
+Name        : FileIO.cpp
+Author      : wchang 00960978
+Prof        : K. Perkins @ CNU
+Version     : 26 Oct 2k19
+Project	    : cs327 - p4: classes, pointers, libraries
+============================================================================*/
 #include <fstream>
+#include <vector>
 #include "../327_proj3_test/includes/FileIO.h"
 #include "../327_proj3_test/includes/constants.h"
 
 using namespace std;
 
-//trys to open filename, and return its contents in contents
-//it will strip out only \n's (char return\line feed)
-//returns:
-//SUCCESS all worked, contents string contains all in the file
-//COULD_NOT_OPEN_FILE_TO_READ contents string is cleared
 int KP_FileIO::getFileContents(const string &filename, string &contents)
 {
-	fstream fin;
+	ifstream fin;
 
 	fin.open(filename.c_str());
 	if(!fin.is_open())
 		return COULD_NOT_OPEN_FILE_TO_READ;
 
-	contents = "";
 	string line = "";
+
 	while(!fin.eof()) {
 		getline(fin, line);
 		contents.append(line);
 	}
+
 	fin.close();
 	return SUCCESS;
 }
 
-//trys to open filename, and serialize  myEntryVector contents to it
-//returns:
-//SUCCESS all worked, filename contains myEntryVector strings
-//COULD_NOT_OPEN_FILE_TO_WRITE
+
 int KP_FileIO::writeVectortoFile(const string filename, vector<string> &myEntryVector)
 {
-	fstream fout;
+	ofstream fout(filename.c_str());
 
-	fout.open(filename.c_str());
 	if(!fout.is_open())
 		return COULD_NOT_OPEN_FILE_TO_WRITE;
 
 	for(int i = 0;i < myEntryVector.size();++i) {
-		if(myEntryVector[i] == "")
-			continue;
 		fout << myEntryVector[i] << endl;
 	}
 	fout.close();
